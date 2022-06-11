d5a51686c3aa67e684d897cbb069a4a1344e0bd8
Alex Resnick <adr8292@gmail.com>
2022-06-11 11:42:52 -0500

    format everything

--- src/main/java/frc/robot/commands/TeleopDrivetrain.java
+++ src/main/java/frc/robot/commands/TeleopDrivetrain.java
@@ -4,47 +4,46 @@
 
 package frc.robot.commands;
 
-// import frc.robot.RobotContainer;
-import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+// import frc.robot.RobotContainer;
+import frc.robot.subsystems.Drivetrain;
 
 /** An example command that uses an example subsystem. */
 public class TeleopDrivetrain extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final Drivetrain drivetrain;
-  private final XboxController driver;
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    private final Drivetrain drivetrain;
+    private final XboxController driver;
 
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public TeleopDrivetrain(Drivetrain drivetrain, XboxController driver) {
-    this.drivetrain = drivetrain;
-    this.driver = driver;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(drivetrain);
-  }
+    /**
+     * Creates a new ExampleCommand.
+     *
+     * @param subsystem The subsystem used by this command.
+     */
+    public TeleopDrivetrain(Drivetrain drivetrain, XboxController driver) {
+        this.drivetrain = drivetrain;
+        this.driver = driver;
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(drivetrain);
+    }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-  }
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    drivetrain.drive(driver.getLeftY()/2, driver.getLeftX()/2, driver.getRightX()/2);
-  }
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        drivetrain.drive(driver.getLeftY() / 2, driver.getLeftX() / 2, driver.getRightX() / 2);
+    }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {}
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 }
