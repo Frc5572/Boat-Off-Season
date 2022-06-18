8472e1e7ac43058462ac2d92ad6b1fa684757165
Alex Resnick <adr8292@gmail.com>
2022-06-18 13:21:00 -0500

    Enable Mecanum & Tank Drive modes (#4)

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -5,13 +5,13 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -21,11 +21,11 @@ import frc.robot.Constants;
  */
 public class Drivetrain extends SubsystemBase {
     /** Creates a new ExampleSubsystem. */
-    MotorController m_frontLeft;
-    MotorController m_rearLeft;
+    CANSparkMax m_frontLeft;
+    CANSparkMax m_rearLeft;
     MotorControllerGroup m_left;
-    MotorController m_frontRight;
-    MotorController m_rearRight;
+    CANSparkMax m_frontRight;
+    CANSparkMax m_rearRight;
     MotorControllerGroup m_right;
     DifferentialDrive tankDrive;
     MecanumDrive mecanumDrive;
@@ -51,13 +51,16 @@ public class Drivetrain extends SubsystemBase {
             new CANSparkMax(Constants.DrivetrainConstants.BACKRIGHTMOTORID, MotorType.kBrushless);
         m_right = new MotorControllerGroup(m_frontRight, m_rearRight);
 
-        // m_right.setInverted(true);
+        m_frontLeft.setIdleMode(IdleMode.kBrake);
+        m_rearLeft.setIdleMode(IdleMode.kBrake);
+        m_frontRight.setIdleMode(IdleMode.kBrake);
+        m_rearRight.setIdleMode(IdleMode.kBrake);
         m_rearRight.setInverted(true);
         m_frontRight.setInverted(true);
 
         mecanum.set(Value.kForward);
 
-        // tankDrive = new DifferentialDrive(m_left, m_right);
+        tankDrive = new DifferentialDrive(m_left, m_right);
         mecanumDrive = new MecanumDrive(m_frontLeft, m_rearLeft, m_frontRight, m_rearRight);
 
     }
@@ -68,18 +71,24 @@ public class Drivetrain extends SubsystemBase {
     }
 
     /**
-     * Default drive function
+     * Tank drive function
+     *
+     * @param left left power
+     * @param right right Power
+     */
+    public void driveTank(double left, double right) {
+        tankDrive.tankDrive(left, right);
+    }
+
+    /**
+     * Mecanum drive function
      *
      * @param yaxis Y-Axis power
      * @param xaxis X-Axis Power
      * @param rotation Rotation Power
      */
-    public void drive(double yaxis, double xaxis, double rotation) {
-        // if(mecanumState) {
-        // tankDrive.tankDrive(left, right);
-        // } else {
+    public void driveMecanum(double yaxis, double xaxis, double rotation) {
         mecanumDrive.driveCartesian(yaxis, xaxis, rotation);
-        // }
     }
 
     @Override
